841cdbe402bba83aea36953cdf73a0cfffa2da24
Garrett Head <garretthead@Garretts-MacBook-Pro.local>
2023-10-30 22:53:20 -0500

    Update FrogCrossing

--- frogCrossingRiver.js
+++ frogCrossingRiver.js
@@ -1,8 +1,10 @@
 /**
     A frog is crossing a river. The river is divided into some number of units,
-    and at each unit, there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
+    and at each unit, there may or may not exist a stone. The frog can jump on a stone,
+    but it must not jump into the water.
 
-    Given a list of stones positions (in units) in sorted ascending order, determine if the frog can cross the river by landing on the last stone.
+    Given a list of stones positions (in units) in sorted ascending order,
+    determine if the frog can cross the river by landing on the last stone.
     Initially, the frog is on the first stone and assumes the first jump must be 1 unit.
 
     If the frog's last jump was k units, its next jump must be either k - 1, k, or k + 1 units.
@@ -22,7 +24,15 @@
     Example 2:
     Input: stones = [0,1,2,3,4,8,9,11]
     Output: false
-    Explanation: There is no way to jump to the last stone as the gap between the 5th and 6th stone is too large.
+    Explanation:
+    There is no way to jump to the last stone as the gap between the 5th and 6th stone is too large.
+
+
+    Constraints:
+    2 <= stones.length <= 2000
+    0 <= stones[i] <= 231 - 1
+    stones[0] == 0
+    stones is sorted in a strictly increasing order.
 */
 
 /**
@@ -30,35 +40,27 @@
  * @return {boolean}
  */
 
-let k = 1;
-var canCross = function(stones) {
+var canCross = function (stones) {
+  let k = 0;
+  const path = stones.reduce((acc, curr, index, arr) => {
+    if (curr === acc) {
+      const nextStone = arr
+      .filter(
+          (x) => 
+          x - curr === k ||
+          x - curr === k - 1 ||
+          x - curr === k + 1
+        )
+        .pop();
+      k = nextStone - curr > k ? nextStone - curr : k;
 
-    // get the furthest stone from the stones array for the valid nextMove.
-    // const nextMove = stones.filter((x) => (x - k === k || x - k === k - 1 || x - k === k + 1)).pop();
-    // const nextStone = stones.lastIndexOf(nextMove);
-    // k = nextMove - k;
-    // console.log('--------');
-    // console.log(stones);
-    // console.log(`nextMove: ${nextMove} nextStone: ${nextStone} k: ${k}`);
+      return nextStone || acc;
+    }
 
-    // stones = stones.slice(nextStone, -1);
-    // console.log(stones);
-
-    // if(stones.length === 1) return true
-    // canCross(stones);
-
-    const jumps = stones.map((val, index, arr) => {
-        return index < arr.length - 1 ? stones[index + 1] - val : 0;
-    });
-
-    console.log(jumps);
-
+    return acc;
+  }, 0);
+  return path === stones.pop()
 };
 
-const stones = [0,1,3,5,6,8,12,17];
+const stones = [0,1,3,5,6,8,12,17] //[0,1,2,3,4,8,9,11];
 console.log(canCross(stones));
-
-
-// iterations
-// 1:   [1,3,5,6,8,12,17]; k = 1; looking for index: 1
-// 2:   
